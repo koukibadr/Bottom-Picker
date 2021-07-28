041a2bcb3a1e8cf540cfd4e9d7db0112b0692199
Kouki Badr <badreddinekouki1998@gmail.com>
2021-07-28 06:34:12 +0100

    create bottom picker layout widgets

--- lib/bottom_picker.dart
+++ lib/bottom_picker.dart
@@ -1,4 +1,5 @@
 import 'package:bottom_picker/widgets/bottom_picker_button.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class BottomPicker extends StatelessWidget {
@@ -25,7 +26,42 @@ class BottomPicker extends StatelessWidget {
           color: Colors.white,
           borderRadius: BorderRadius.only(
               topRight: Radius.circular(20), topLeft: Radius.circular(20))),
-      child: Center(child: BottomPickerButton()),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Choose trip date"),
+                  Icon(Icons.close, color: Colors.black, size: 20)
+                ],
+              ),
+            ),
+            Container(
+              height: 150,
+              width: 150,
+              child: CupertinoPicker(
+                  itemExtent: 40,
+                  onSelectedItemChanged: (int) {},
+                  children: [
+                    Text("data"),
+                    Text("data"),
+                    Text("data"),
+                    Text("data")
+                  ]),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                BottomPickerButton(),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
