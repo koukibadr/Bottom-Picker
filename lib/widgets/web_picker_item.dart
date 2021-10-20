71e813d85f326c7f922d7f11e0b7c016c07f0bba
Kouki Badr <badreddinekouki1998@gmail.com>
2021-10-20 05:51:05 +0100

    add check on web platform and display date picker

---
+++ lib/widgets/web_picker_item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WebPickerItem extends StatelessWidget {
+  const WebPickerItem({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
